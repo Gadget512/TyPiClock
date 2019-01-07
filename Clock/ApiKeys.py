120a6557141aabc9e269fbdae23f4eefad9cea02
GadgeT512 <tyler.j.brinks@gmail.com>
2019-01-07 12:08:42 -0700

    More file cleanup

--- Clock/ApiKeys.py
+++
@@ -1,6 +0,0 @@
-# change this to your API keys
-# Weather Underground API key
-wuapi = '32ee89a66950a457'
-# Google Maps API key
-#googleapi = 'AIzaSyDRp8M841R_scksGlk7d8JVvceAfEStqVQ'
-googleapi = ''
